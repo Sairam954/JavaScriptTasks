282bd8afb0efec88befc17091f3f572af9b6ac48
Sairam_Srivatsavai <Sairam_Srivatsavai@epam.com>
2017-08-01 19:50:06 +0530

    Home Assignment 3

--- HW1/rentCarCost.js
+++ HW1/rentCarCost.js
@@ -1,9 +1,9 @@
 function rentalCarCost(d) {
- let costPerDay=40,
-     numberOfDays=d,
-     totalCost,
-     discount;
-  discount = numberOfDays>=7 ? 50 : (numberOfDays >=3 ? 20 : 0 );  
-  totalCost = numberOfDays*costPerDay-discount;
- return totalCost;
+    let costPerDay = 40,
+        numberOfDays = d,
+        totalCost,
+        discount;
+    discount = numberOfDays >= 7 ? 50 : (numberOfDays >= 3 ? 20 : 0);
+    totalCost = numberOfDays * costPerDay - discount;
+    return totalCost;
 }
