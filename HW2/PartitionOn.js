9b81ea4f488f1e5f2737f7222e07b63124943e07
Sairam_Srivatsavai <Sairam_Srivatsavai@epam.com>
2017-08-09 15:56:38 +0530

    Refactored HW2 and HW 4

--- HW2/PartitionOn.js
+++ HW2/PartitionOn.js
@@ -1,17 +1,12 @@
 function partitionOn(pred, items) {
-    let trueItems = items.filter(pred),s
+    let trueItems = items.filter(pred),
         falseItems = [];
     items.forEach(function (item) {
-
         if (!pred(item)) {
             falseItems.push(item);
         }
-
-
     })
-
     items.length = 0;
     items.push.apply(items, falseItems.concat(trueItems));
-
     return falseItems.length;
 }
